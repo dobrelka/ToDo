ff4a60854ca90dda15c356b45fb630b893265fd2
dobrelka <dobrelyatest@gmail.com>
2020-06-28 12:05:13 +0300

    Adding onViewCreated toolbar.title and toolbar.setOnMenuItemClickListener

--- app/src/main/java/com/commonsware/todo/RosterListFragment.kt
+++ app/src/main/java/com/commonsware/todo/RosterListFragment.kt
@@ -1,10 +1,12 @@
 package com.commonsware.todo
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class RosterListFragment : Fragment() {
     override fun onCreateView(
@@ -15,5 +17,17 @@ class RosterListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
       super.onViewCreated(view, savedInstanceState)
+
+      toolbar.title = getString(R.string.app_name)
+      toolbar.inflateMenu(R.menu.actions)
+
+      toolbar.setOnMenuItemClickListener { item ->
+        when (item.itemId) {
+          R.id.about -> startActivity(Intent(activity, AboutActivity::class.java))
+          else -> return@setOnMenuItemClickListener false
+        }
+
+        true
+      }
     }
 }
