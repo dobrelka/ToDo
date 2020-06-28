ac996ab428039b681b506316f3d9b35e0441d1fc
dobrelka <dobrelyatest@gmail.com>
2020-06-28 11:20:59 +0300

    Created RosterListFragment.kt in com.commonsware.todo

---
+++ app/src/main/java/com/commonsware/todo/RosterListFragment.kt
@@ -0,0 +1,15 @@
+package com.commonsware.todo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class RosterListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.activity_main, container, false)
+}
